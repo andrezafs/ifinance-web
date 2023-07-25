6a1d02d97cb2dd879827ba86e4d86e75f58d7023
Andreza Ferreira <andrezafs.rr@gmail.com>
2023-07-25 14:38:12 -0300

    feat: ✨ add dark mode

--- src/modules/category/pages/Categories.tsx
+++ src/modules/category/pages/Categories.tsx
@@ -1,13 +1,17 @@
-import { Col } from 'antd';
+import { Col, theme } from 'antd';
 import { Header } from 'antd/es/layout/layout';
 
 import { HeaderActions } from '../components/HeaderActions';
-import { TableCategories } from '../components/TableCategories';
 import { ModalCreateCategory } from '../components/ModalCreateCategory';
 import { ModalDeleteCategory } from '../components/ModalDeleteCategory';
 import { ModalEditCategory } from '../components/ModalEditCategory';
+import { TableCategories } from '../components/TableCategories';
 
 export function Categories() {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
     <>
       <Col
@@ -23,7 +27,7 @@ export function Categories() {
             alignItems: 'center',
             display: 'flex',
             width: '100%',
-            backgroundColor: '#fff',
+            background: colorBgContainer,
           }}
         >
           <HeaderActions />
