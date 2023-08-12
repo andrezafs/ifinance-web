8a9c130b5112caa02c0caa23d470ecfcabfab85a
Andreza Ferreira <andrezafs.rr@gmail.com>
2023-08-12 14:07:08 -0300

    feat: ✨ add delete credit card

--- src/modules/credit-card/pages/CreditCads.tsx
+++ src/modules/credit-card/pages/CreditCads.tsx
@@ -6,6 +6,7 @@ import { useListCreditCardsQuery } from '@/graphql';
 import { HeaderActions } from '../components/Header';
 import { ModalCreateCreditCard } from '../components/ModalCreateCreditCard';
 import { CardCreditCard } from '../components/CardCreditCard';
+import { ModalDeleteCreditCard } from '../components/ModalDeleteCreditCard';
 
 export function CreditCards() {
   const { data } = useListCreditCardsQuery();
@@ -25,120 +26,15 @@ export function CreditCards() {
       </Header>
 
       <Row gutter={[16, 8]}>
-        {/* <Col span={8}>
-          <CardCreditCard
-            id="1"
-            title="Nubank Douglas"
-            closingDay="06 de Julho "
-            bank={{
-              name: 'Nubank',
-              logo: 'nubank.png',
-              color: '#8A05BE',
-            }}
-            currentInvoice={1000}
-            availableLimit={1000}
-            totalLimit={5000}
-            progress={100}
-            paymentDate="06 de Agosto"
-            usedLimit={4000}
-          />
-        </Col>
-        <Col span={8}>
-          <CardCreditCard
-            id="2"
-            title="Nubank Andreza"
-            closingDay="01 de Julho "
-            bank={{
-              name: 'Nubank',
-              logo: 'nubank.png',
-              color: '#8A05BE',
-            }}
-            currentInvoice={1000}
-            availableLimit={1000}
-            totalLimit={5000}
-            progress={100}
-            paymentDate="07 de Julho"
-            usedLimit={4000}
-          />
-        </Col>
-        <Col span={8}>
-          <CardCreditCard
-            id="3"
-            title="Inter"
-            closingDay="01 de Julho "
-            bank={{
-              name: 'Inter',
-              logo: 'inter.png',
-              color: '#FF8700',
-            }}
-            currentInvoice={1000}
-            availableLimit={1000}
-            totalLimit={5000}
-            progress={100}
-            paymentDate="07 de Julho"
-            usedLimit={4000}
-          />
-        </Col>
-        <Col span={8}>
-          <CardCreditCard
-            id="4"
-            title="NEO"
-            closingDay="01 de Julho "
-            bank={{
-              name: 'NEO',
-              logo: 'neo.png',
-              color: '#ff0000',
-            }}
-            currentInvoice={1000}
-            availableLimit={1000}
-            totalLimit={5000}
-            progress={100}
-            paymentDate="07 de Julho"
-            usedLimit={4000}
-          />
-        </Col>
-        <Col span={8}>
-          <CardCreditCard
-            id="5"
-            title="Neon"
-            closingDay="01 de Julho "
-            bank={{
-              name: 'Neon',
-              logo: 'neon.png',
-              color: '#00ffff',
-            }}
-            currentInvoice={1000}
-            availableLimit={1000}
-            totalLimit={5000}
-            progress={100}
-            paymentDate="07 de Julho"
-            usedLimit={4000}
-          />
-        </Col> */}
-
-        {data?.listCreditCards.map(creditCards => (
-          <Col span={8} key={creditCards.id}>
-            <CardCreditCard
-              title={creditCards.name}
-              closingDay={creditCards.closingDay}
-              bank={{
-                name: creditCards.bank.name,
-                logo: creditCards.bank.image,
-                color: creditCards.bank.color,
-              }}
-              currentInvoice={0}
-              availableLimit={creditCards.limitAvailable}
-              totalLimit={creditCards.limit}
-              progress={50}
-              paymentDate={creditCards.dueDay}
-              usedLimit={creditCards.limitUsed}
-              id={creditCards.id}
-            />
+        {data?.listCreditCards.map(creditCard => (
+          <Col span={8} key={creditCard.id}>
+            <CardCreditCard data={creditCard} />
           </Col>
         ))}
       </Row>
 
       <ModalCreateCreditCard />
+      <ModalDeleteCreditCard />
     </>
   );
 }
